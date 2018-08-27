e695f50be291d933d502d15eb401242ba015361f
梁港林 <392517304@qq.com>
2018-08-27 16:48:51 +0800

    fix some bug about iqiyi

--- video_scrapy/spiders/video_spider.py
+++ video_scrapy/spiders/video_spider.py
@@ -9,9 +9,11 @@ from video_scrapy.settings import my_defined_urls
 
 class YoutubeDlSpider(scrapy.Spider):
     name = 'video'
-    start_urls = ['http://www.iqiyi.com/v_19rr2aesjs.html']
+    start_urls = ['http://www.iqiyi.com/v_19rqyrk8ic.html']
     youtube_dl_not_you_get = False
     get_playlist = False
+    handle_httpstatus_list = [404]
+    iqiyi_id = {}
 
     def start_requests(self):
         if self.youtube_dl_not_you_get:
@@ -36,12 +38,19 @@ class YoutubeDlSpider(scrapy.Spider):
                         filetype = str(j).rsplit(".", 1)[-1]
                         yield scrapy.Request(url=my_url_dict[j], callback=self.savefile, meta={"name": name, "filetype": filetype, "fileid": None, "id": None, "end": None})
         else:
+            iqiyi_url = []
             for i in self.start_urls:
                 if "bilibili" in i:
                     yield scrapy.Request(url=i, callback=self.bili_parse)
                     self.start_urls.remove(i)
-                else:
-                    pass
+                elif "iqiyi.com" in i:
+                    iqiyi_url.append(i)
+                    self.start_urls.remove(i)
+            if len(iqiyi_url) == 0:
+                pass
+            else:
+                for i in self.iqiyi_url_process(iqiyi_url):
+                    yield i
             if len(self.start_urls) == 0:
                 pass
             else:
@@ -80,6 +89,60 @@ class YoutubeDlSpider(scrapy.Spider):
                                 end = False
                             yield scrapy.Request(url=my_url_dict[j], callback=self.savefile, meta={"name": name, "filetype": filetype, "fileid": j, "id": None, "end": end})
 
+    def check_iqiyi_finish(self, name):
+        self.iqiyi_id[name].setdefault("get_num", 0)
+        if "send_num" in self.iqiyi_id[name]:
+            if int(self.iqiyi_id[name]["send_num"]) == int(self.iqiyi_id[name]["get_num"]):
+                if len(self.iqiyi_id[name].setdefault("error", [])) == 0:
+                    pass
+                else:
+                    self.iqiyi_id[name]["get_num"] = 0
+                    self.iqiyi_id[name]["error_num"] = len(
+                        self.iqiyi_id[name]["error"])
+                    self.iqiyi_id[name].pop("send_num")
+                    return True
+        return False
+
+    def iqiyi_url_process(self, my_iqiyi_url):
+        print("waiting for you_get get iqiyi_urls")
+        from video_scrapy.you_get.common import main
+        for iqiyi_url in my_iqiyi_url:
+            iqiyi_url = [iqiyi_url]
+            main(my_own_urls=iqiyi_url,
+                 my_own_playlist=self.get_playlist)
+            print("get iqiyi_urls finish")
+            if "error" in my_defined_urls:
+                print(
+                    "can't get urls for some videos,please look at error.txt for more information!")
+                error = my_defined_urls.pop("error")
+                import datetime
+                nowTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                with open("error.txt", "a") as f:
+                    f.write('\n')
+                    f.write(nowTime)
+                    f.write('\n')
+                    f.write("\n".join(error))
+            my_temp = list(my_defined_urls.keys())
+            for i in my_temp:
+                my_url_dict = my_defined_urls[i]
+                name = str(i)
+                self.iqiyi_id.setdefault(name, {}).setdefault("url", iqiyi_url)
+                filetype = my_url_dict.pop("filetype")
+                end_id = len(my_url_dict)
+                if end_id == 1:
+                    url = my_url_dict.popitem()[1]
+                    filetype = re.search(r"\.(\w+?)\?", url).group(1)
+                    if filetype == "m3u8":
+                        yield scrapy.Request(url=url, callback=self.parse_m3u8, meta={"name": name, "iqiyi": None})
+                else:
+                    for j in my_url_dict:
+                        if int(j) == int(end_id):
+                            end = True
+                        else:
+                            end = False
+                        yield scrapy.Request(url=my_url_dict[j], callback=self.savefile, meta={"name": name, "filetype": filetype, "fileid": j, "id": None, "end": end})
+                my_defined_urls.pop(i)
+
     def parse_m3u8(self, response):
         url = response.url
         name = response.meta['name']
@@ -96,24 +159,48 @@ class YoutubeDlSpider(scrapy.Spider):
             for index, line in enumerate(file_line):
                 if "EXTINF" in line:
                     unknow = False
-                    if file_line[index+1][0:4]=="http":
-                        pd_url=file_line[index+1]
+                    if file_line[index + 1][0:4] == "http":
+                        pd_url = file_line[index + 1]
                     else:
                         if file_line[index + 1][0] != '/':
                             pd_url = url.rsplit(
                                 "/", 1)[0] + "/" + file_line[index + 1]
                         else:
-                            pd_url = url.rsplit("/", 1)[0] + file_line[index + 1]
-                    yield scrapy.Request(pd_url, callback=self.savefile,
-                                         meta={'fileid': i, 'name': name, 'end': False, "id": None, "filetype": "ts"})
+                            pd_url = url.rsplit(
+                                "/", 1)[0] + file_line[index + 1]
+                    if "iqiyi" in response.meta:
+                        if len(self.iqiyi_id.setdefault(name, {}).setdefault("error", [])) == 0:
+                            if self.iqiyi_id.setdefault(name, {}).setdefault("error_num", 0) == 0:
+                                yield scrapy.Request(pd_url, callback=self.savefile,
+                                                     meta={'fileid': int(i), 'name': name, 'end': False, "id": None, "filetype": "ts", "iqiyi": None})
+                            else:
+                                pass
+                        else:
+                            if int(i) in self.iqiyi_id.setdefault(name, {}).setdefault("error", []):
+                                yield scrapy.Request(pd_url, callback=self.savefile,
+                                                     meta={'fileid': int(i), 'name': name, 'end': False, "id": None, "filetype": "ts", "iqiyi": None})
+                            else:
+                                pass
+                    else:
+                        yield scrapy.Request(pd_url, callback=self.savefile,
+                                             meta={'fileid': int(i), 'name': name, 'end': False, "id": None, "filetype": "ts"})
                     i = i + 1
                 if "ENDLIST" in line:
+                    if "iqiyi" in response.meta:
+                        if self.iqiyi_id.setdefault(name, {}).setdefault("error_num", 0) != 0:
+                            self.iqiyi_id[name]["send_num"] = self.iqiyi_id[
+                                name]["error_num"]
+                        else:
+                            self.iqiyi_id[name]["send_num"] = i - 1
+                        if self.check_iqiyi_finish(name):
+                            for k in self.iqiyi_url_process(self.iqiyi_id[name]["url"]):
+                                yield k
                     item = FileItem()
                     item["id"] = None
                     item['fileid'] = i
                     item['name'] = name
                     item['end'] = True
-                    item['content'] = ''
+                    item['content'] = b''
                     item['filetype'] = 'ts'
                     yield item
             if unknow:
@@ -223,5 +310,24 @@ class YoutubeDlSpider(scrapy.Spider):
         item['name'] = str(response.meta['name']).encode(
         ).translate(None, b'\\/:*?"<>|').decode()
         item['filetype'] = response.meta['filetype']
-        item['content'] = response.body
-        yield item
+        if "iqiyi" in response.meta:
+            self.iqiyi_id[response.meta["name"]]["get_num"] = self.iqiyi_id[
+                response.meta["name"]].setdefault("get_num", 0) + 1
+            if int(response.status) == 404:
+                if int(response.meta['fileid']) in self.iqiyi_id[response.meta["name"]].setdefault("error", []):
+                    pass
+                else:
+                    self.iqiyi_id[response.meta["name"]].setdefault(
+                        "error", []).append(int(response.meta["fileid"]))
+            else:
+                if int(response.meta["fileid"]) in self.iqiyi_id[response.meta["name"]].setdefault("error", []):
+                    self.iqiyi_id[response.meta["name"]][
+                        "error"].remove(int(response.meta["fileid"]))
+                item['content'] = response.body
+                yield item
+            if self.check_iqiyi_finish(response.meta["name"]):
+                for i in self.iqiyi_url_process(self.iqiyi_id[response.meta["name"]]["url"]):
+                    yield i
+        else:
+            item['content'] = response.body
+            yield item
